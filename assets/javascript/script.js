78f4142532ab2be0186311016f938488c79b78b9
anthonyfdunphy <anthonyfdunphy@gmail.com>
2022-05-31 16:00:37 +0000

    for loop created to check month for film schedule

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,44 @@
+/* array of month names*/
+const monthByName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December']
+
 /* code to work out which week of the month it is */
 const d = new Date();
 const date = d.getDate();
 const day = d.getDay();
 const weekOfMonth = Math.ceil((date - 1 - day) / 7);
+const month = d.getMonth();
 
-const filmTitles = ['Batman', 'Ambulance', 'The Reason I Jump', 'Harry Potter']
 
-document.getElementById("top-h1").innerHTML = filmTitles[0];
-document.getElementById("bottom-h1").innerHTML = filmTitles[1];
+const filmSchedule = [
+    {
+        startMonth: 'March', 
+        titleTop: 'MarchBatman', 
+        titleBottom: 'MarchAmbulance'
+    },
+    {
+        startMonth: 'April', 
+        titleTop: 'AprilBatman', 
+        titleBottom: 'AprilAmbulance'
+    },
+    {
+        startMonth: 'May', 
+        titleTop: 'MayBatman', 
+        titleBottom: 'MayAmbulance'},
+    {
+        startMonth: 'June', 
+        titleTop: 'JuneBatman', 
+        titleBottom: 'JuneAmbulance'
+    },
+    {
+        startMonth: 'July', 
+        titleTop: 'JulyBatman', 
+        titleBottom: 'JulyAmbulance'
+    },
+]
+
+/* loop to cycle through array and match current month to film title*/
+for (var i =0; i < filmSchedule.length; i++){
+    if(filmSchedule[i].startMonth == monthByName[month]){
+        document.getElementById('top-h1').innerHTML = filmSchedule[i].titleTop;
+    }
+}
